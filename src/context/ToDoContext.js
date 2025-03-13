2b74fdf16d719df94c6b1fd17b3d0adb409f453e
Ayush0115D <dhakreayush578@gmail.com>
2025-03-13 23:55:51 +0530

    for exporting todocontext

--- src/context/ToDoContext.js
+++ src/context/ToDoContext.js
@@ -1,5 +1,20 @@
 import { createContext,useContext } from "react";
-export const TodoContext=createContext({})
+export const TodoContext=createContext({
+    todos:[  //context and  redux ke functionality app.jsx me likhe jate
+        {
+    
+        id:1,
+        todo:"Todo msg",
+        completed:false,
+    }
+    ],
+     addTodo:(todo)=>{},  //methods and value...functionality app.jsx me likhege
+     updatedTodo:(id,todo0)=>{},
+     deleteTodo:(id) => {},
+     toggleComplete:(id)=>{}
+})
+
+
 export const useTodo=()=>{
     return useContext(TodoContext)
 }
